970ca88a2abc8a4b2a967deb6ad5345e26009605
ollie0603 <olliie2102@gmail.com>
2025-03-28 11:42:04 -0300

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -19,7 +19,7 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'showdown',
-	host: 'api.showdownpathways.dynv6.net',
+	host: 'api-showdownpathways.dynv6.net',
 	port: 443,
 	httpport: 80,
 	altport: 80,
